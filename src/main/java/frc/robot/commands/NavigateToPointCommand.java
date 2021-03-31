571349ed3b31b55c09a24fa1f974c081c5af539b
Admin <tobiasalam@gmail.com>
2021-03-30 20:29:16 -0400

    basic autonav setup

--- src/main/java/frc/robot/commands/NavigateToPointCommand.java
+++ src/main/java/frc/robot/commands/NavigateToPointCommand.java
@@ -2,27 +2,32 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.SwerveSubsystem;
-import java.util.Queue;
+import java.util.ArrayList;
 import frc.robot.util.Point;
 
 public class NavigateToPointCommand extends CommandBase
 {
-  public Queue<Point> points;
+  public ArrayList<Point> points;
+  public Point currentPoint;
+  public Point targetPoint;
+
   private SwerveSubsystem swerveSubsystem;
+  private final double EPSILON = 0.1;
+  private boolean noMorePoints = false;
 
   public NavigateToPointCommand(SwerveSubsystem swerveSubsystem)
   {
     this.swerveSubsystem = swerveSubsystem;
-
+    this.currentPoint = new Point(0.0,0.0);
     addRequirements(swerveSubsystem);
   }
 
   @Override public void execute()
   {
-    swerveSubsystem.drive(0,0,0);
+    System.out.println("ok");
   }
 
   @Override public boolean isFinished() {
-    return points.size() == 0;
+    return noMorePoints;
   }
 }
