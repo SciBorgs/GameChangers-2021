a12436abb02505fab2e1ccbc2d1089c8eea85db4
SciBorgs <60524929+sciborgsweb@users.noreply.github.com>
2021-04-01 13:16:48 -0400

    Field Centric and robot centric swerve drive work!

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -2,16 +2,23 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
+import frc.robot.util.PID;
+import frc.robot.hardware.SciPigeon;
 import frc.robot.commands.SwerveJoystickCommand;
 import frc.robot.hardware.SciAbsoluteEncoder;
 import frc.robot.hardware.SciSpark;
 import frc.robot.subsystems.SwerveSubsystem;
-import frc.robot.util.PID;
 
 public class Robot extends TimedRobot
 {
   private SwerveSubsystem swerveSubsystem;
   private SwerveJoystickCommand swerveJoystickCommand;
+  private NetworkTableInstance networkTables;
+  private NetworkTable visionTable;
   final double STEERING_SPARK_GEAR_RATIO    = 1.0 / 60;
   final double STEERING_ENCODER_GEAR_RATIO  = 1.0 / 5;
   final double INTAKE_FLYWHEEL_GEAR_RATIO   = 1.0 / 9;
@@ -24,6 +31,8 @@ public class Robot extends TimedRobot
   private SciAbsoluteEncoder steeringEncoder;
 
   private PID steeringAnglePID;
+  
+  //private SciPigeon pigeon;
 
   @Override public void robotInit()
   {
@@ -42,9 +51,13 @@ public class Robot extends TimedRobot
 
     // steeringEncoder = new SciAbsoluteEncoder(3,
     //                                          STEERING_ENCODER_GEAR_RATIO,
-    //                                          Math.toRadians(90),
+    //                                          0,
     //                                          false);
     // steeringAnglePID = new PID(.82, 0.0, 0.0);
+    //pigeon = new SciPigeon(20);
+
+    //networkTables = NetworkTableInstance.getDefault();
+    //visionTable = networkTables.getTable("Vision");
   }
 
   @Override public void robotPeriodic()
@@ -64,15 +77,21 @@ public class Robot extends TimedRobot
 
   @Override public void teleopInit()
   {
-    swerveSubsystem.setZero();
+    //pigeon.setAngle(0);
     swerveJoystickCommand.schedule();
   }
 
   @Override public void teleopPeriodic() 
   {  
+    //System.out.println(visionTable.getEntry("Port Lateral Position").getDouble(-1));
+    //System.out.println(visionTable.getEntry("Port Longitudinal Position").getDouble(-1));
+    //System.out.println(visionTable.getEntry("Ball Lateral Position").getDouble(-1));
+    //System.out.println(visionTable.getEntry("Ball Longitudinal Position").getDouble(-1));
     //double output = steeringAnglePID.getOutput(Math.toRadians(270), steeringEncoder.getAngle());
     //steeringSpark1.set(output);
-    //System.out.println("getAngle(): " + Math.toDegrees(steeringEncoder.getAngle())+"\t output:" + output);
+    //System.out.println("getAngle(): " + Math.toDegrees(steeringEncoder.getAngle()));
+
+    //System.out.println("Pigeon Yaw Angle: " + Math.toDegrees(pigeon.getAngle()));
     
     //hopperSpark.set(.1);
     //shooterSpark1.set(.1);
